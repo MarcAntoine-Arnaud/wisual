e646c44733cd07678ee264c55764a7d6f6bd4338
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2015-05-03 19:11:09 +0200

    add route to list server files and directories

--- wisual/views.py
+++ wisual/views.py
@@ -1,6 +1,9 @@
 
+import os
+
 from wisual import g_app
 from flask import render_template
+from flask import jsonify, abort
 
 @g_app.route("/")
 def index():
@@ -9,3 +12,20 @@ def index():
 @g_app.route("/psnr")
 def computePsnr():
 	return render_template('psnr.html')
+
+@g_app.route("/ls/<path:path>")
+def ls(path):
+	files = []
+	dirs = []
+	try:
+		filesAndDirs = os.listdir("/" + path)
+		for f in filesAndDirs:
+			absPath = os.path.join("/" + path, f)
+			if os.path.isdir( absPath ):
+				dirs.append(f)
+			else:
+				files.append(f)
+			print f, os.path.isdir(absPath)
+	except:
+		abort(404)
+	return jsonify({"files":files, "directories": dirs, "path":path})
