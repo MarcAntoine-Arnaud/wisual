631a92ed286dd3e012bd07a24b3c11d93e3571b2
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2015-05-23 20:33:35 +0200

    update application to display curves with results

--- wisual/views.py
+++ wisual/views.py
@@ -4,33 +4,60 @@ import xmltodict
 from wisual import g_app
 from flask import render_template, jsonify, abort
 
+navigation = [
+	{
+		"caption": "New Job",
+		"href" : "/job"
+	},
+	{
+		"caption": "List Job",
+		"href" : "/jobs"
+	},
+	{
+		"caption": "View Results",
+		"href" : "/results"
+	}
+]
+
+analysisModes = [
+	{
+		"id": "psnr",
+		"name": "PSNR"
+	},
+	{
+		"id": "ssim",
+		"name": "SSIM"
+	},
+	{
+		"id": "avgpsnr",
+		"name": "Average PSNR"
+	},
+	{
+		"id": "avgssim",
+		"name": "Average SSIM"
+	}
+]
 
 @g_app.route("/")
 def index():
-	return render_template('index.html')
+	return render_template('index.html', navigation=navigation)
 
-@g_app.route("/psnr")
-def computePsnr():
-	return render_template('psnr.html')
+@g_app.route("/job")
+def newJob():
+	return render_template('job.html', navigation=navigation, analysisModes=analysisModes)
 
-@g_app.route("/psnr2")
-def compute2Psnr():
-	return render_template('psnr2.html')
+@g_app.route("/jobs")
+def listAllJobs():
+	return render_template('jobs.html', navigation=navigation)
 
-@g_app.route("/ssim")
-def computeSsim():
-	return render_template('ssim.html')
+@g_app.route("/results")
+def getAllResults():
+	resultsFiles = os.listdir("results")
+	return render_template('results.html', navigation=navigation, resultsFiles=resultsFiles)
 
-@g_app.route("/testarbo")
-def computeTestarbo():
-	return render_template('test_arbo.html')
-
-	
-@g_app.route("/test")
-def computeTest():
-	result={'analyse':{'frame':[{'0':120},{'1':130}]}}
-	return jsonify(result)
-
+@g_app.route("/results/<path:resultFile>")
+def getResult(resultFile):
+	return render_template('result.html', navigation=navigation, resultFile=resultFile)
 
 @g_app.route("/ls/<path:path>")
 def ls(path):
@@ -44,19 +71,13 @@ def ls(path):
 				dirs.append(f)
 			else:
 				files.append(f)
-			print f, os.path.isdir(absPath)
+			#print f, os.path.isdir(absPath)
 	except:
 		abort(404)
 	return jsonify({"files":files, "directories": dirs, "path":path})
 
-@g_app.route("/results")
-def getResultsList():
-	files = os.listdir("results")
-	print files
-	return jsonify({"files":files})
-
-@g_app.route("/results/<path:path>")
-def getResult(path):
+@g_app.route("/results/<path:path>/data")
+def getResultData(path):
 	f = open( os.path.join("results", path), 'r')
 	output = xmltodict.parse(f, process_namespaces=False)
 	f.close()
